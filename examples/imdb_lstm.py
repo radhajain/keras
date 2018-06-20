90f03f6d3d499ba5b1753db867b6fdc16578253d
radhajain <jainr@stanford.edu>
2018-06-20 14:29:47 -0700

    Added wandb logging tools to imdb_lstm

--- examples/imdb_lstm.py
+++ examples/imdb_lstm.py
@@ -20,25 +20,41 @@ from keras.layers import Dense, Embedding
 from keras.layers import LSTM
 from keras.datasets import imdb
 
-max_features = 20000
+import wandb
+from wandb.keras import WandbCallback
+
+# logging code
+run = wandb.init()
+config = run.config
+config.epochs = 10
+config.batch_size = 33
+config.dropout = 0.29
+config.max_features = 2500
+
+
+
+
+# max_features = 20000
 maxlen = 80  # cut texts after this number of words (among top max_features most common words)
-batch_size = 32
+# batch_size = 32
 
 print('Loading data...')
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=config.max_features)
 print(len(x_train), 'train sequences')
 print(len(x_test), 'test sequences')
+
 
 print('Pad sequences (samples x time)')
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
 x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
 print('x_train shape:', x_train.shape)
 print('x_test shape:', x_test.shape)
+labels = ["Positive", "Negative"]
 
 print('Build model...')
 model = Sequential()
-model.add(Embedding(max_features, 128))
-model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+model.add(Embedding(config.max_features, 128))
+model.add(LSTM(128, dropout=config.dropout, recurrent_dropout=config.dropout))
 model.add(Dense(1, activation='sigmoid'))
 
 # try using different optimizers and different optimizer configs
@@ -46,12 +62,19 @@ model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
+model.summary()
+
+
+# create a lambda callback to call log
+
 print('Train...')
 model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=15,
-          validation_data=(x_test, y_test))
+          batch_size=config.batch_size,
+          epochs=config.epochs,
+          validation_data=(x_test, y_test), 
+          callbacks=[WandbCallback()])
+
 score, acc = model.evaluate(x_test, y_test,
-                            batch_size=batch_size)
+                            batch_size=config.batch_size)
 print('Test score:', score)
 print('Test accuracy:', acc)
